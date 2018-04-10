ca2261a60b20f57c903084969b416a6f2ea548ca
truong.nguyen <truong.nguyen@everymatrix.com>
2018-04-10 17:36:34 +0700

    - draft resourceFactory

---
+++ src/utils/most-resource.ts
@@ -0,0 +1,100 @@
+import {Stream} from "most";
+import {create, ISelfEmitStream} from "./most";
+import * as objHash from "object-hash";
+
+interface IClassType<T> {
+    new(...arg: any[]): T;
+}
+
+interface IQuery {
+    [key: string]: number | string | boolean,
+}
+
+interface IResourceFactoryOption<T extends IClassType<T>, K extends IQuery> {
+    name: string,
+    model: T,
+    sampleQuery: K,
+    cacheInvalidator: (output: T[]) => boolean,
+    cacheStorage?: ICacheStore<T>,
+}
+
+interface IResourceFactoryOutput<T extends IClassType<T>, K extends IQuery> {
+    queryStream: ISelfEmitStream<K>,
+    responseStream: Stream<Promise<T[]>>
+}
+
+export interface ICacheStore<T extends IClassType<T>> {
+    getSingle: (q: IQuery) => T[],
+    getIntersect: (q: IQuery[]) => T[],
+    getUnion: (q: IQuery[]) => T[],
+    add: (q: IQuery, v: T[]) => void,
+}
+
+class CacheStore<T extends IClassType<T>> implements ICacheStore<T> {
+    private cache: WeakMap<IQuery, T[]>;
+    private hashQueryMap: {[key: string]: IQuery} = {};
+
+    constructor() {
+        this.cache = new WeakMap();
+    }
+
+    public getSingle(q: IQuery): T[] {
+        const hash: string = objHash(q);
+        return this.getByHash(hash) || [];
+    }
+
+    public getIntersect(query: IQuery[]): T[] {
+        return query.map(q => this.getSingle(q))
+            .reduce((r, s) => r.concat(s), []);
+    }
+
+    public getUnion(query: IQuery[]): T[] {
+        const sorted: T[][] = query.map(q => this.getSingle(q)).sort((a, b) => a.length - b.length);
+        return sorted.reduce((res, current: T[]) => {
+            if (!res.length) {
+                return current;
+            }
+            return res.filter(i => current.some(c => c === i));
+        }, []);
+    }
+
+    public add(q: IQuery, v: T[]): void {
+        const hash: string = objHash(q);
+        const cachedQuery = this.hashQueryMap[hash];
+        if (cachedQuery) {
+            const cachedTArray = this.cache.get(cachedQuery);
+            this.cache.set(cachedQuery, (cachedTArray || []).concat(v))
+        } else {
+            this.hashQueryMap[hash] = q;
+            this.cache.set(q, v);
+        }
+    }
+
+    private getByHash(hash: string): T[] | undefined {
+        const cachedQuery = this.hashQueryMap[hash];
+        if (cachedQuery) {
+            return this.cache.get(cachedQuery);
+        }
+        return undefined;
+    }
+}
+
+export function resourceFactory<T extends IClassType<T>, K extends IQuery>(opts: IResourceFactoryOption<T, K>):
+    IResourceFactoryOutput<T, K> {
+
+    const cacheStore: ICacheStore<T> = opts.cacheStorage || new CacheStore<T>();
+
+    const queryStream = create<K>(opts.name);
+    const responseStream: Stream<Promise<T[]>> = queryStream.map((q: K) => {
+        const response = cacheStore.getSingle(q);
+        if (opts.cacheInvalidator(response)) {
+            return Promise.resolve(response);
+        }
+        return Promise.resolve([]);
+    });
+
+    return {
+        queryStream,
+        responseStream,
+    }
+}
